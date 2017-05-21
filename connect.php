db71b6db8f4ab1f7fda9b7efc1701ab426eff535
cratosbaba <tobi.sowole@gmail.com>
2017-05-21 22:27:57 +0100

    My initial commit

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$database = 'geolanda';
+$user     = 'postgres';
+$password = 'iloveJesus';
+
+//include libraries
+//include("adodb5/adodb-exceptions.inc.php");
+//include("adodb5/adodb.inc.php");
+
+
+?>
